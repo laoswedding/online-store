bf0c7b7ed8c43768d1eac6e13bf8e5bf925801c3
laoswedding <111362320+laoswedding@users.noreply.github.com>
2026-09-14 14:47:24 +0700

    add qr

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,4 +1,7 @@
 const orderItemsEl = document.querySelector(".order-items");
+const orderTotalEl = document.querySelector(".order-total");
+const orderItemsTotalEl = document.querySelector(".order-items-total");
+const paymentAmountEl = document.querySelector(".payment-amount");
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 
 //render order summary
@@ -7,16 +10,31 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function renderOrderItems() {
+  let totalPrice = 0,
+    totalItems = 0;
+
+  cart.forEach((item) => {
+    totalPrice += item.price * item.numberOfUnits;
+    totalItems += item.numberOfUnits;
+  });
+
   orderItemsEl.innerHTML = ""; // clear order element
 
   cart.forEach((item) => {
     orderItemsEl.innerHTML += `
         <div class="order-item">
-
                 <img src="../${item.imgSrc}" alt="${item.name}" class="order-item-img">
                 <p class="order-item-description"><span class="order-item-name">${item.name}:</span> ${item.description} <span class="order-item-qty"> <br>Qty: ${item.numberOfUnits}</span></p>
-                <p class="order-item-price">${item.price * item.numberOfUnits} LAK</p></div>`;
+                <p class="order-item-price">${(
+                  item.price * item.numberOfUnits
+                ).toLocaleString("lo-LA", {
+                  style: "currency",
+                  currency: "LAK",
+                })} LAK</p></div>`;
   });
+  orderItemsTotalEl.innerHTML = totalItems;
+  orderTotalEl.innerHTML = totalPrice.toLocaleString("lo-LA");
+  paymentAmountEl.innerHTML = totalPrice.toLocaleString("lo-LA");
 }
 
 // Choose shipping company
